740e6b47ce256fab588da3fdab2aeb28038b1922
Shota Fukuoka <s.fukuoka.23.2.789@4nonome.com>
2023-10-08 15:54:21 +0900

    関数の提出

--- prime_number.py
+++ prime_number.py
@@ -1,16 +1,14 @@
-a = int(input("aの値を入力: "))
-b = int(input("bの値を入力: "))
+def prime_number(n):
+        for k in range(2,n):
+                if n % k == 0:
+                        return False
+                        break
+        else:
+                return True
 
-for k in range(2,a):
-    if a % k == 0:
-        print(f'{a}は{k}を約数にもつから、素数でない')
-        break
+n = float(input("nの値を入力: "))
+if not n.is_integer() or n<=0:
+        print("error")
 else:
-        print(f'{a}は素数')
-
-for k in range(2,b):
-    if b % k == 0:
-        print(f'{b}は{k}を約数にもつから、素数でない')
-        break
-else:
-        print(f'{b}は素数！')
+        result=prime_number(int(n))
+        print(result)
