e5d170460f1fe6cd95a527481901fe7bc551d705
Shota Fukuoka <s.fukuoka.23.2.789@4nonome.com>
2023-10-09 20:03:04 +0900

    制御構文の課題修正

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,5 +1,10 @@
 # TODO
 ε=float(1)
-while 1+ε>1:
-    ε=ε*(1/2)
-    print(ε)
+while 1.0+ε>1.0:
+    beferε=ε
+    ε=ε*(1.0/2.0)
+
+print(beferε)
+    
+
+
--- prime_number.py
+++ prime_number.py
@@ -1,16 +1,29 @@
-a = int(input("aの値を入力: "))
-b = int(input("bの値を入力: "))
+def prime_number(n):
+        for k in range(2,n):
+                if n % k == 0:
+                        return False
+                        break
+        else:
+                return True
 
-for k in range(2,a):
-    if a % k == 0:
-        print(f'{a}は{k}を約数にもつから、素数でない')
-        break
-else:
-        print(f'{a}は素数')
+n=int(input("nの値を入力:"))
+result=prime_number(n)
+print(result)
 
-for k in range(2,b):
-    if b % k == 0:
-        print(f'{b}は{k}を約数にもつから、素数でない')
-        break
-else:
-        print(f'{b}は素数！')
+
+# a = int(input("aの値を入力: "))
+# b = int(input("bの値を入力: "))
+
+# for k in range(2,a):
+#     if a % k == 0:
+#         print(f'{a}は{k}を約数にもつから、素数でない')
+#         break
+# else:
+#         print(f'{a}は素数')
+
+# for k in range(2,b):
+#     if b % k == 0:
+#         print(f'{b}は{k}を約数にもつから、素数でない')
+#         break
+# else:
+#         print(f'{b}は素数！')
